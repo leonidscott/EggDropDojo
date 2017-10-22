63e076bbe39c47c71707a6c03704cc850206a149
Dogxx000 <scot0530@morris.umn.edu>
2017-10-22 18:53:31 -0500

    Add Unit Tests for Egg

--- src/UtilsTest/TestEgg.java
+++ src/UtilsTest/TestEgg.java
@@ -1,12 +1,22 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestEgg {
+	
+	Egg egg = new Egg();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCount () {
+		assertEquals(2, egg.count());
+	}
+	
+	@Test
+	public void testOneEgg() throws EmptyNestException {
+		EggDrop eggDrop = new EggDrop(100, 50);
+		eggDrop.drop(egg, 25);
+		assertEquals(1, egg.count());
 	}
 
 }
