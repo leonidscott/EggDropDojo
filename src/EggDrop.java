9c6c51ef5625e4d1fb49765fbd48bb353a68607a
Dogxx000 <scot0530@morris.umn.edu>
2017-10-22 19:42:02 -0500

    Fill in EggDrop.java

--- src/EggDrop.java
+++ src/EggDrop.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class EggDrop {
     private int[] building;
     private int breakFloor;
@@ -5,15 +7,29 @@ public class EggDrop {
     //Main Constructor
     public EggDrop(int n) {
         building = new int[n];
+        Random random = new Random();
+        random.nextInt(n);
     }
     //Constructor for tests cases
     public EggDrop(int n, int breakFloor) {
         building = new int[n];
+        this.breakFloor = breakFloor;
     }
 
 
     public boolean drop(Egg egg, int floor) throws EmptyNestException {
-		return false;
-
+		checkEmpty(egg);
+		if(floor != breakFloor) {
+			egg.decriment();
+			checkEmpty(egg);
+			return false;
+		}
+		return true;
+    }
+    
+    private void checkEmpty(Egg egg) throws EmptyNestException {
+    	if(egg.count() < 1){
+    		throw new EmptyNestException();
+    	}
     }
 }
